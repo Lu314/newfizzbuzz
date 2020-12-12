d9b19d108a3013c5910185d50371e17bb2384ae9
Lu <l.scalzo90@gmail.com>
2020-12-12 03:24:48 -0600

    Add files via upload

---
+++ Fizz Buzz.py
@@ -0,0 +1,20 @@
+
+
+num = 1
+
+while (num <=100):
+    if(num % 3 == 0) and (num % 5 == 0):
+        print(str(num) +". Fizzbuzz!")
+    
+    elif (num % 3 == 0):
+        print(str(num) +". Fizz!")
+    
+    elif(num % 5 == 0):
+        print(str(num) +". Buzz!")
+
+    else:
+        print(num)
+
+
+    
+    num +=1
